13afbfbfb8787fc548a4205fff937461d3b5a697
Shalin85 <shalin2385@gmail.com>
2022-09-11 08:51:24 +0530

    to test a java program

---
+++ MySecondProject/src/MySecondClass.java
@@ -0,0 +1,39 @@
+
+public class MySecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte b=1;
+		b=127;
+		//byte takes values from -128 to 127
+		short a=25;
+		int z=100;
+		long m=10000;
+		float f=100.1f;
+		float g=50;
+		double d=2000;
+		double e=150000.12;
+		char c='i';
+		char j='2';
+		boolean t=true;
+		boolean x=false;
+		
+		
+		System.out.println(b);
+		System.out.println(a);
+		System.out.println(z);
+		System.out.println(m);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(c);
+		System.out.println(j);
+		System.out.println(t);
+		System.out.println(x);
+		
+		
+	}
+
+}
